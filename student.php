cea3c9cdbe31869acf3a0a16a9f9eceac51bac6d
kevinmwalugha <kevinmwalugha@gmial.com>
2015-07-23 19:16:19 +0300

    initial commit

---
+++ student.php
@@ -0,0 +1,4 @@
+//A student exam system where a student send a text where he/she enters her index number
+//He/she gets a text reply displaying his/her exam results
+//Database with table student
+//Records fname,lname,index_no(unique),subject and grades, overall grade
